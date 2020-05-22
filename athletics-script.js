19d2fbed7b7c807307a107b3c7aa89422136b83c
Patrick Lee <Bituminous@users.noreply.github.com>
2020-05-21 23:27:47 -0700

    Picture issue resolved

--- athletics-script.js
+++ athletics-script.js
@@ -1,56 +1,56 @@
 $(".sport").hover(function(){
   if (this.id === 'b-Cross-Country'){
     $('.SeasonalSlide').css({
-      'background': 'url(Athletic-Pictures/b-Cross-Country-banner.jpg)',
+      'background': 'url(Athletic-pictures/b-Cross-Country-banner.jpg)',
       'background-size': 'cover',
       'background-position': '50% 0'
     })
   }
   if (this.id === 'g-Cross-Country'){
     $('.SeasonalSlide').css({
-      'background': 'url(Athletic-Pictures/g-Cross-Country-banner.jpg)',
+      'background': 'url(Athletic-pictures/g-Cross-Country-banner.jpg)',
       'background-size': 'cover',
       'background-position': '50% 20%'
     })
   }
   if (this.id === 'Football'){
     $('.SeasonalSlide').css({
-      'background': 'url(Athletic-Pictures/football-banner.jpg)',
+      'background': 'url(Athletic-pictures/football-banner.jpg)',
       'background-size': 'cover',
       'background-position': '50% 20%'
     })
   }
   if (this.id === 'g-Golf'){
     $('.SeasonalSlide').css({
-      'background': 'url(Athletic-Pictures/g-Golf-banner.jpg)',
+      'background': 'url(Athletic-pictures/g-Golf-banner.jpg)',
       'background-size': 'cover',
       'background-position': '50% 35%'
     })
   }
   if (this.id === 'g-Tennis'){
     $('.SeasonalSlide').css({
-      'background': 'url(Athletic-Pictures/g-Tennis-banner.jpg)',
+      'background': 'url(Athletic-pictures/g-Tennis-banner.jpg)',
       'background-size': 'cover',
       'background-position': '50% 20%'
     })
   }
   if (this.id === 'g-Volleyball'){
     $('.SeasonalSlide').css({
-      'background': 'url(Athletic-Pictures/g-Volleyball-banner.jpg)',
+      'background': 'url(Athletic-pictures/g-Volleyball-banner.jpg)',
       'background-size': 'cover',
       'background-position': '50% 20%'
     })
   }
   if (this.id === 'b-Water-Polo'){
     $('.SeasonalSlide').css({
-      'background': 'url(Athletic-Pictures/b-Water-Polo-banner.jpg)',
+      'background': 'url(Athletic-pictures/b-Water-Polo-banner.jpg)',
       'background-size': 'cover',
       'background-position': '50% 20%'
     })
   }
   if (this.id === 'Cheer'){
     $('.SeasonalSlide').css({
-      'background': 'url(Athletic-Pictures/Cheer-banner.jpg)',
+      'background': 'url(Athletic-pictures/Cheer-banner.jpg)',
       'background-size': 'cover',
       'background-position': '50% 20%'
     })
